30241b5e45c385df5f7bdb2cc6b19d3c6000aaab
NeoTuruu <neo.orgil@gmail.com>
2024-06-02 14:13:10 +0800

    state bolon search controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,25 @@
 require("@babel/polyfill");
 import Search from "./model/Search1.js";
 
-let search = new Search("pasta");
+// web app-n tuluv 1. Hailtiin query, ur dun 2.Tuhain uzuulj bga jor 3. Likelsan joruud 4. Zahialj baigaa joriin nairlaga
+const state = {};
+const controlSearch = async () => {
+  // 1. webees hailtiin tulhuur ugiig gargaj avna
+  const query = "pizza";
 
-search.doSearch().then((r) => console.log(r));
+  if (query) {
+    //   2. Shineer hailtiin objectiig uusgej ugnu
+    state.search = new Search(query);
+    //   3. Hialt hiihed zoriulj delgetsiig UI beltgene
+    //   4. Hailtiig guitsetgene
+    await state.search.doSearch();
+    //   5. Hailtiin ur dung delgetsend uzuulne
+    console.log(state.search.result);
+  }
+
+  console.log("daragdlaa");
+};
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
