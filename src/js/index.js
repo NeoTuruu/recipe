cedbfbce573b6b1ab9949b56a12d244696b2f4c4
NeoTuruu <neo.orgil@gmail.com>
2024-06-03 02:17:42 +0800

    search iconiig gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,32 @@
 require("@babel/polyfill");
 import Search from "./model/Search1.js";
+import { elements, loader, renderLoader, clearLoader } from "./view/base.js";
+import * as searchView from "./view/searchView1";
 
 // web app-n tuluv 1. Hailtiin query, ur dun 2.Tuhain uzuulj bga jor 3. Likelsan joruud 4. Zahialj baigaa joriin nairlaga
 const state = {};
 const controlSearch = async () => {
   // 1. webees hailtiin tulhuur ugiig gargaj avna
-  const query = "pizza";
+  const query = searchView.getInput();
 
   if (query) {
     //   2. Shineer hailtiin objectiig uusgej ugnu
     state.search = new Search(query);
     //   3. Hialt hiihed zoriulj delgetsiig UI beltgene
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
     //   4. Hailtiig guitsetgene
     await state.search.doSearch();
     //   5. Hailtiin ur dung delgetsend uzuulne
-    console.log(state.search.result);
+    clearLoader();
+    if (state.search.result === undefined) alert("Хайлтаар илэрцгүй");
+    else searchView.renderRecipes(state.search.result);
   }
 
   console.log("daragdlaa");
 };
-document.querySelector(".search").addEventListener("submit", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
